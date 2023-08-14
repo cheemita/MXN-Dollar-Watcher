9d2ec7ca5e32e1246afd772b9f18f8b98288dee5
José Manuel <gjosemanuel375@gmail.com>
2023-08-14 12:30:16 -0600

    Funcionando 100%

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,50 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 
-import TaskList from "../components/TasksList";
 
-const HomeScreen = () => {
+
+const HomeScreen = ({ navigation }) => {
   return (
     <Layout>
-      <TaskList />
+
+      <View style={{ flex: 1, marginTop: 20, }}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("TaskFormScreen")}
+          style={styles.chooseButton1}>
+          <Text style={styles.bbbb1}>CHART ON LIVE</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("BuySellScreen")}
+          style={styles.chooseButton2}>
+          <Text style={styles.bbbb2}>FOREIGN EXCHANGES</Text>
+        </TouchableOpacity>
+
+      </View>
+
     </Layout>
   );
 };
+
+
+const styles = StyleSheet.create({
+
+  bbbb1: {
+    fontSize: 20,
+  },
+  bbbb2: {
+    fontSize: 20,
+  },
+  chooseButton1: {
+    alignItems: "center",
+    paddingTop: "1%"
+
+  },
+  chooseButton2: {
+    alignItems: "center",
+    paddingTop: "5%"
+  },
+
+});
 
 export default HomeScreen;
