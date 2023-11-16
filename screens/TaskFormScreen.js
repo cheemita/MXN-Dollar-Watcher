29fd96eaba1898d1b0f5377a753ed26db67b296c
José Manuel <gjosemanuel375@gmail.com>
2023-11-15 23:29:02 -0600

    Funcionando en IOS con node_modules actualizado. Se cambió el link de la grafica en tiempo real

--- screens/TaskFormScreen.js
+++ screens/TaskFormScreen.js
@@ -6,7 +6,7 @@ const TaskFormScreen = ({ navigation, route }) => {
   return (
     <View style={{ flex: 1 }}>
       <WebView
-        source={{ uri: 'https://www.tradingview.com/widgetembed/?frameElementId=tradingview_228f8&symbol=FX%3AUSDMXN&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=f1f3f6&backgroundColor=%23ffffff&gridColor=rgba(42%2C 46%2C 57%2C 0.06)&studies=[]&theme=light&style=1&timezone=Etc%2FUTC&studies_overrides={}&overrides={}&enabled_features=[]&disabled_features=[]&locale=en#{"page-uri"%3A"www.tradingview.com%2Fwidget%2Fadvanced-chart%2F"}' }}
+        source={{ uri: 'https://www.tradingview.com/widgetembed/?frameElementId=tradingview_228f8&symbol=FX%3AUSDMXN&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=f1f3f6&backgroundColor=%23ffffff&gridColor=rgba(42%2C' }}
         style={{ flex: 1 }}
       />
     </View>
